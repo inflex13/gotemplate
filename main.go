c3c414a1f75ca406c6cb086612467d6f4b6c6592
Ethan Pusppa <inflex13@gmail.com>
2017-02-16 14:25:36 -0500

    Implemented httprouter package with ability to accept url params

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
-package gotemplate
+package main
+
+import (
+	"github.com/alecthomas/template"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+var tpl *template.Template
+
+type Person struct {
+	Name string
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/about", About)
+	router.GET("/user/:name", User)
+
+	http.ListenAndServe(":8080", router)
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func About(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	tpl.ExecuteTemplate(w, "about.gohtml", nil)
+}
+
+func User(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	Agent := Person{
+		p.ByName("name"),
+	}
+
+	tpl.ExecuteTemplate(w, "user.gohtml", Agent)
+}
